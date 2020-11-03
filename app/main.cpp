7743499594dc28f3c200a039674cd318b8c71f15
ThomasR <trouch@prophesee.ai>
2020-11-03 10:40:29 +0100

    First opengl legacy triangle

--- app/main.cpp
+++ app/main.cpp
@@ -4,7 +4,45 @@
  * 2020 Thomas Rouch                                                                                                 *
  *********************************************************************************************************************/
 
+#include <GLFW/glfw3.h>
+
 int main(int argc, const char *argv[])
 {
+    GLFWwindow *window;
+
+    // Init the Lib
+    if (!glfwInit())
+        return 1;
+
+    // Create named window and its opengl context
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return 2;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Loop util the user closesthe window
+    while (!glfwWindowShouldClose(window))
+    {
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
